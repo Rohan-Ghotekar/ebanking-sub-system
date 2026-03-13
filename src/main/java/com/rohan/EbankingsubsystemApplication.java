fce2a2ebaba701d86195fe9a270a4af41e582b02
Rohan Ghotekar <rohanghotekar@outlook.com>
2026-03-13 11:23:15 +0530

    backend api

---
+++ src/main/java/com/rohan/EbankingsubsystemApplication.java
@@ -0,0 +1,16 @@
+package com.rohan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class EbankingsubsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbankingsubsystemApplication.class, args);
+		
+	}
+
+}
