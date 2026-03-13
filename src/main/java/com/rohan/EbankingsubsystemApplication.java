153e22e50a477e9cc9a8816029297b76d3711b2b
Rohan Ghotekar <rohanghotekar@outlook.com>
2026-03-13 13:02:53 +0530

    Updated backend service and docker configuration

--- src/main/java/com/rohan/EbankingsubsystemApplication.java
+++ src/main/java/com/rohan/EbankingsubsystemApplication.java
@@ -2,15 +2,14 @@ package com.rohan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class EbankingsubsystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EbankingsubsystemApplication.class, args);
-		
 	}
 
 }
